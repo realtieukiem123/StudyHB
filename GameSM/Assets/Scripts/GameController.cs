c45f9b41baf9aa70cd90d3321c1ad4f014b8b496
realtieukiem123 <realtieukiem@gmail.com>
2023-03-21 00:47:50 +0700

    sua 29/03

---
+++ GameSM/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public List<GameObject> listBrick = new List<GameObject>();
+    public static GameController Instance { get; set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+   
+}
--- GameSM/Assets/Scripts/Movement.cs
+++ GameSM/Assets/Scripts/Movement.cs
@@ -6,6 +6,11 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed;
+    private bool isMove;
+
+    private Vector3 _firstCubePos;
+    private Vector3 _currentCubePos;
+    private int _cubeListIndexCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,21 +20,56 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || MobileInput.Instance.swipeLeft)
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || MobileInput.Instance.swipeLeft && !isMove)
         {
+            isMove = true;
             rb.velocity = Vector3.left * speed * Time.deltaTime;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) || MobileInput.Instance.swipeRight)
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || MobileInput.Instance.swipeRight && !isMove)
         {
+            isMove = true;
             rb.velocity = Vector3.right * speed * Time.deltaTime;
         }
-        else if(Input.GetKeyDown(KeyCode.UpArrow) || MobileInput.Instance.swipeUp)
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || MobileInput.Instance.swipeUp && !isMove)
         {
+            isMove = true;
             rb.velocity = Vector3.forward * speed * Time.deltaTime;
         }
-        else if(Input.GetKeyDown(KeyCode.DownArrow) || MobileInput.Instance.swipeDown)
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || MobileInput.Instance.swipeDown && !isMove)
         {
+            isMove = true;
             rb.velocity = Vector3.forward * -speed * Time.deltaTime;
+        }
+        if (rb.velocity == Vector3.zero)
+        {
+            isMove = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Brick"))
+        {
+            var lBrick = GameController.Instance.listBrick;
+            lBrick.Add(other.gameObject);
+            if (lBrick.Count == 1)
+            {
+                var pos = GetComponent<MeshRenderer>().bounds.max;
+                pos.y += 4f;
+                _firstCubePos = pos;
+                //_firstCubePos = GetComponent<MeshRenderer>().bounds.max;
+                _currentCubePos = new Vector3(other.transform.position.x, _firstCubePos.y, other.transform.position.z);
+                other.gameObject.transform.position = _currentCubePos;
+                _currentCubePos = new Vector3(other.transform.position.x, transform.position.y + 0.3f, other.transform.position.z);
+                other.gameObject.GetComponent<Brick>().UpdateCubePosition(transform, true);
+            }
+            else if (lBrick.Count > 1)
+            {
+                other.gameObject.transform.position = _currentCubePos;
+                _currentCubePos = new Vector3(other.transform.position.x, other.gameObject.transform.position.y + 0.3f, other.transform.position.z);
+                other.gameObject.GetComponent<Brick>().UpdateCubePosition(lBrick[_cubeListIndexCounter].transform, true);
+                _cubeListIndexCounter++;
+            }
         }
     }
 }
